3f9408be03e2873893fb76c1419b0d2b04ca19f7
RoboRambotnik <sq1p1t2me@gmail.com>
2021-02-25 23:09:49 +0100

    removed arithmetic macros - renamed errors & refactored tests accordingly - refactored ordering of parameters in usage of to_u256 to be consistent with ordering of fn parameters - replaced expectations with ok_or

--- src/amm.rs
+++ src/amm.rs
@@ -1,12 +1,10 @@
 use core::convert::TryFrom;
 use primitive_types::U256;
-use crate::MathError::{DivisorIsZero, ResultOverflow, InsufficientBuyReserve};
+use crate::MathError::{ZeroInReserve, Overflow, InsufficientOutReserve};
 
 type Balance = u128;
 
 const FIXED_ROUND_UP: Balance = 1;
-const CANNOT_OVERFLOW: &str = "Cannot overflow";
-const DENOMINATOR_NOT_ZERO: &str = "Cannot panic as denominator cannot be 0";
 
 macro_rules! ensure {
     ($e:expr, $f:expr) => {
@@ -48,37 +46,12 @@ macro_rules! to_u128 {
     };
 }
 
-macro_rules! multiply {
-    ($a:expr, $b:expr) => {
-        $a.checked_mul($b).expect(CANNOT_OVERFLOW)
-    };
-}
-
-macro_rules! add {
-    ($a:expr, $b:expr) => {
-        $a.checked_add($b).expect(CANNOT_OVERFLOW)
-    };
-}
-
-macro_rules! divide {
-    ($a:expr, $b:expr) => {
-        $a.checked_div($b).expect(DENOMINATOR_NOT_ZERO)
-    };
-}
-
-macro_rules! substract {
-    ($a:expr, $b:expr) => {
-        $a.checked_sub($b).expect(CANNOT_OVERFLOW)
-    };
-}
-
-
 #[derive(PartialEq)]
 #[derive(Debug)]
 pub enum MathError {
-    DivisorIsZero,
-    ResultOverflow,
-    InsufficientBuyReserve,
+    ZeroInReserve,
+    Overflow,
+    InsufficientOutReserve,
 }
 
 /// Calculating spot price given reserve of selling asset and reserve of buying asset.
@@ -90,17 +63,19 @@ pub enum MathError {
 ///
 /// Returns MathError in case of error
 pub fn calculate_spot_price(sell_reserve: Balance, buy_reserve: Balance, amount: Balance) -> Result<Balance, MathError> {
-    ensure!(sell_reserve != 0, DivisorIsZero);
+    ensure!(sell_reserve != 0, ZeroInReserve);
 
     if amount == 0 || buy_reserve == 0 {
         return Ok(0u128);
     }
 
-    let (amount_hp, buy_reserve_hp, sell_reserve_hp) = to_u256!(amount, buy_reserve, sell_reserve);
-    let dividend = multiply!(buy_reserve_hp, amount_hp);
-    let spot_price_hp = divide!(dividend, sell_reserve_hp);
+    let (sell_reserve_hp, buy_reserve_hp, amount_hp) = to_u256!(sell_reserve, buy_reserve, amount);
 
-    to_u128!(spot_price_hp).ok_or(ResultOverflow)
+    let spot_price_hp = buy_reserve_hp
+        .checked_mul(amount_hp).ok_or(Overflow).unwrap()
+        .checked_div(sell_reserve_hp).ok_or(Overflow).unwrap();
+
+    to_u128!(spot_price_hp).ok_or(Overflow)
 }
 
 /// Calculating selling price given reserve of selling asset and reserve of buying asset.
@@ -112,15 +87,16 @@ pub fn calculate_spot_price(sell_reserve: Balance, buy_reserve: Balance, amount:
 ///
 /// Returns MathError in case of error
 pub fn calculate_sell_price(sell_reserve: Balance, buy_reserve: Balance, sell_amount: Balance) -> Result<Balance, MathError> {
-    let (sell_amount_hp, buy_reserve_hp, sell_reserve_hp) = to_u256!(sell_amount, buy_reserve, sell_reserve);
-    let divisor = add!(sell_reserve_hp, sell_amount_hp);
-    ensure!(!divisor.is_zero(), DivisorIsZero);
+    let (sell_reserve_hp, buy_reserve_hp, sell_amount_hp) = to_u256!(sell_reserve, buy_reserve, sell_amount);
 
-    let dividend = multiply!(buy_reserve_hp, sell_amount_hp);
-    let sale_price_hp = divide!(dividend, divisor);
+    let denominator = sell_reserve_hp.checked_add(sell_amount_hp).expect("cannot");
+    ensure!(!denominator.is_zero(), ZeroInReserve);
 
-    let result = to_u128!(sale_price_hp).ok_or(ResultOverflow).ok();
-    round_up!(result.unwrap()).ok_or(ResultOverflow)
+    let numerator = buy_reserve_hp.checked_mul(sell_amount_hp).ok_or(Overflow).unwrap();
+    let sale_price_hp = numerator.checked_div(denominator).ok_or(Overflow).unwrap();
+
+    let result = to_u128!(sale_price_hp).ok_or(Overflow).ok();
+    round_up!(result.unwrap()).ok_or(Overflow)
 }
 
 /// Calculating buying price given reserve of selling asset and reserve of buying asset.
@@ -132,27 +108,29 @@ pub fn calculate_sell_price(sell_reserve: Balance, buy_reserve: Balance, sell_am
 ///
 /// Returns None in case of error
 pub fn calculate_buy_price(sell_reserve: Balance, buy_reserve: Balance, amount: Balance) -> Result<Balance, MathError> {
-    ensure!(amount <= buy_reserve, InsufficientBuyReserve);
+    ensure!(amount <= buy_reserve, InsufficientOutReserve);
 
-    let (amount_hp, buy_reserve_hp, sell_reserve_hp) = to_u256!(amount, buy_reserve, sell_reserve);
-    let divisor = substract!(buy_reserve_hp, amount_hp);
-    ensure!(!divisor.is_zero(), DivisorIsZero);
+    let (sell_reserve_hp, buy_reserve_hp, amount_hp) = to_u256!(sell_reserve, buy_reserve, amount);
 
-    let dividend = multiply!(sell_reserve_hp, amount_hp);
-    let buy_price_hp = divide!(dividend, divisor);
+    let numerator = sell_reserve_hp.checked_mul(amount_hp).ok_or(Overflow).unwrap();
+    let denominator = buy_reserve_hp.checked_sub(amount_hp).ok_or(Overflow).unwrap();
+    ensure!(!denominator.is_zero(), ZeroInReserve);
+    let buy_price_hp = numerator.checked_div(denominator).ok_or(Overflow).unwrap();
 
-    let result = to_u128!(buy_price_hp).ok_or(ResultOverflow).ok();
-    round_up!(result.unwrap()).ok_or(ResultOverflow)
+    let result = to_u128!(buy_price_hp).ok_or(Overflow).ok();
+    round_up!(result.unwrap()).ok_or(Overflow)
 }
 
 pub fn calculate_liquidity_in(asset_a_reserve: Balance, asset_b_reserve: Balance, amount: Balance) -> Result<Balance, MathError> {
-    ensure!(asset_a_reserve != 0, DivisorIsZero);
+    ensure!(asset_a_reserve != 0, ZeroInReserve);
 
     let (a_reserve_hp, b_reserve_hp, amount_hp) = to_u256!(asset_a_reserve, asset_b_reserve, amount);
-    let dividend = multiply!(amount_hp, b_reserve_hp);
-    let b_required_hp = divide!(dividend, a_reserve_hp);
 
-    to_u128!(b_required_hp).ok_or(ResultOverflow)
+    let b_required_hp = amount_hp
+        .checked_mul(b_reserve_hp).ok_or(Overflow).unwrap()
+        .checked_div(a_reserve_hp).ok_or(Overflow).unwrap();
+
+    to_u128!(b_required_hp).ok_or(Overflow)
 }
 
 pub fn calculate_liquidity_out(
@@ -161,18 +139,22 @@ pub fn calculate_liquidity_out(
     amount: Balance,
     total_liquidity: Balance,
 ) -> Result<(Balance, Balance), MathError> {
-    ensure!(total_liquidity != 0, DivisorIsZero);
+    ensure!(total_liquidity != 0, ZeroInReserve);
 
     let (a_reserve_hp, b_reserve_hp, amount_hp, liquidity_hp) =
         to_u256!(asset_a_reserve, asset_b_reserve, amount, total_liquidity);
 
-    let dividend_a = multiply!(amount_hp, a_reserve_hp);
-    let remove_amount_a_hp = divide!(dividend_a, liquidity_hp);
-    let remove_amount_a = to_u128!(remove_amount_a_hp).ok_or(ResultOverflow);
+    let remove_amount_a_hp = amount_hp
+        .checked_mul(a_reserve_hp).ok_or(Overflow).unwrap()
+        .checked_div(liquidity_hp).ok_or(Overflow).unwrap();
 
-    let dividend_b = multiply!(amount_hp, b_reserve_hp);
-    let remove_amount_b_hp = divide!(dividend_b, liquidity_hp);
-    let remove_amount_b = to_u128!(remove_amount_b_hp).ok_or(ResultOverflow);
+    let remove_amount_a = to_u128!(remove_amount_a_hp).ok_or(Overflow);
+
+    let remove_amount_b_hp = b_reserve_hp
+        .checked_mul(amount_hp).ok_or(Overflow).unwrap()
+        .checked_div(liquidity_hp).ok_or(Overflow).unwrap();
+
+    let remove_amount_b = to_u128!(remove_amount_b_hp).ok_or(Overflow);
 
     Ok((remove_amount_a.unwrap(), remove_amount_b.unwrap()))
 }
